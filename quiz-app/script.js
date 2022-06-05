c0f3c832fbfa328c9271c19dbcfc34892f3cba64
oladips <oladips200@gmail.com>
2022-06-05 11:14:19 +0000

    added getting answer function and deselect function

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -11,17 +11,17 @@ const quizData = [
   {
     question: "What is the capital of USA?",
     a: "New Delhi",
-    b: "Mumbai",
+    b: "Peru",
     c: "los angeles",
     d: "new york",
-    correct: "b",
+    correct: "c",
   },
   {
     question: "What is the best laptop product company?",
     a: "Apple",
-    b: "Dell",
+    b: "Lenovo",
     c: "HP",
-    d: "Lenovo",
+    d: "Dell",
     correct: "b",
   },
   {
@@ -30,7 +30,7 @@ const quizData = [
     b: "Samsung",
     c: "Nokia",
     d: "LG",
-    correct: "b",
+    correct: "a",
   },
   {
     question: "What does HTML stands for?",
@@ -42,11 +42,11 @@ const quizData = [
   },
   {
     question: "what year was javasript launched",
-    a: "1995",
+    a: "1997",
     b: "1996",
-    c: "1997",
+    c: "1995",
     d: "1998",
-    correct: "a",
+    correct: "c",
   },
 ];
 
@@ -56,10 +56,13 @@ const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submitBtn");
+const answersEl = document.querySelectorAll(".answer");
 let currrentQuiz = 0;
-loadQuiz();
+let score = 0;
 
+loadQuiz();
 function loadQuiz() {
+  deselctAll();
   const currentQuizData = quizData[currrentQuiz];
   question.innerText = currentQuizData.question;
   (a_text.innerText = currentQuizData.a),
@@ -69,15 +72,35 @@ function loadQuiz() {
 }
 
 function getSelected() {
-  
+  let answer = undefined;
+  answersEl.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+function deselctAll() {
+  answersEl.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
 }
 
 submitBtn.addEventListener("click", () => {
-  currrentQuiz++;
-  if (currrentQuiz < quizData.length) {
-    loadQuiz();
+  const answer = getSelected();
+  answer === quizData[currrentQuiz].correct && score++;
+  console.log(answer);
+  if (answer) {
+    currrentQuiz++;
+    if (currrentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      // TODO create an alert box
+      alert("uve finished");
+      console.log(score);
+    }
   } else {
-    // TODO create an alert box
-    alert("uve finished");
+    alert("please select an answer");
   }
 });
