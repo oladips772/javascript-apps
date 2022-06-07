f75d462eb2b8efd6ddcecb10922e2d5575d4c26b
oladips <oladips200@gmail.com>
2022-06-07 21:32:43 +0000

    add meal to local storage working remove meal from local storage working

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,15 +1,16 @@
 /** @format */
 const searchTerm = document.getElementById("search_term").value;
-
 async function getRandomMeal() {
   const resp = await fetch(
     "https://www.themealdb.com/api/json/v1/1/random.php"
   );
   const responseData = await resp.json();
   const randomMeal = responseData.meals[0];
-  console.log(randomMeal);
   addMeal(randomMeal);
 }
+
+getRandomMeal();
+getFavoriteMeals();
 
 function addMeal(mealData) {
   const randomContainer = document.getElementById("random_container");
@@ -18,27 +19,65 @@ function addMeal(mealData) {
 
   if (mealData) {
     randomMealDiv.innerHTML = `
-          <p>Random Meal</p>
-          <img src="${mealData.strMealThumb}" alt="" />
-          <span>'${mealData.strMeal}'</span>
-          <i class="fa fa-heart heart" id="heart" aria-hidden="true"></i>
+    <p>Random Meal</p>
+    <img src="${mealData.strMealThumb}" alt="" />
+    <span>'${mealData.strMeal}'</span>
+    <i class="fa fa-heart heart" id="heart" aria-hidden="true"></i>
     `;
-    randomContainer.appendChild(randomMealDiv);
   }
+
+  randomContainer.appendChild(randomMealDiv);
 
   const heartBtn = randomMealDiv.querySelector(".heart");
   heartBtn.addEventListener("click", () => {
-    heartBtn.classList.toggle("active");
+    if (heartBtn.classList.contains("active")) {
+      heartBtn.classList.remove("active");
+      removeMealFromLocalStorage(mealData.idMeal);
+    } else {
+      addMealsToLocalStorage(mealData.idMeal);
+      heartBtn.classList.add("active");
+    }
   });
 }
 
-getRandomMeal();
+function removeMealFromLocalStorage(mealId) {
+  const mealsId = getMealsFromLocalStorage();
+  mealsId.splice(mealsId.indexOf(mealId), 1);
+  localStorage.setItem("mealsId", JSON.stringify(mealsId));
+  getFavoriteMeals();
+}
 
-// async function getMealById(id) {
-//   const response = await fetch(
-//     "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
-//   );
-// }
+function addMealsToLocalStorage(mealId) {
+  const mealsId = getMealsFromLocalStorage();
+  mealsId.push(mealId);
+  localStorage.setItem("mealsId", JSON.stringify(mealsId));
+  getFavoriteMeals();
+}
+
+function getMealsFromLocalStorage() {
+  const mealsId = JSON.parse(localStorage.getItem("mealsId"));
+  return mealsId ? mealsId : [];
+}
+
+async function getMealById(id) {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+  const responseData = await response.json();
+  const meal = responseData.meals[0];
+  return meal;
+}
+
+async function getFavoriteMeals() {
+  const mealsId = getMealsFromLocalStorage();
+  const meals = [];
+  for (i = 0; i < mealsId.length; i++) {
+    const mealId = mealsId[i];
+    const meal = await getMealById(mealId);
+    meals.push(meal);
+  }
+  console.log(meals);
+}
 
 // async function getMealBySearch(searchTerm) {
 //   const response = await fetch(
