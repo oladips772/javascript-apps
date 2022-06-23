2f36ed6724f5407abc31810560e10cc5f6bdfae3
oladips <oladips200@gmail.com>
2022-06-23 21:46:54 +0000

    user data now showing all functions working fine

--- old-apps/github-profile-app/script.js
+++ old-apps/github-profile-app/script.js
@@ -1,5 +1,14 @@
 /** @format */
+const profileCard = document.getElementById("profile_card");
+const searchTerm = document.getElementById("search");
+const searchBtn = document.querySelector("#searchBtn");
 const GIT_URL = `https://api.github.com/users/`;
+
+searchBtn.onclick = () => {
+  profileCard.innerHTML = "";
+  showUser();
+  searchTerm.value = "";
+};
 
 async function getUser(user) {
   const respData = await fetch(`${GIT_URL}${user}`);
@@ -8,4 +17,36 @@ async function getUser(user) {
   return userData;
 }
 
-getUser("oladips772");
+async function showUser() {
+  const userData = await getUser(searchTerm.value);
+  const userCard = document.createElement("div");
+  userCard.classList = "user_card";
+  if (userData) {
+    userCard.innerHTML = `
+  <img
+            src=${userData.avatar_url}
+            alt=""
+          />
+          <div class="stats_div">
+            <h3>${userData.name}</h3>
+            <div class="stat">
+              <i class="fa fa-star"></i>
+              // <span>{}</span>
+            </div>
+            <div class="stat">
+              <i class="fa-solid fa-users"></i>
+              <span>${userData.followers}</span>
+            </div>
+            <div class="stat">
+              <i class="fa-solid fa-address-card"></i>
+              <span>${userData.bio}</span>
+            </div>
+          </div>
+  `;
+  } else if (userData.message === "Not Found") {
+    userCard.innerHTML = `
+      <h3>User not found</h3>
+    `;
+  }
+  profileCard.appendChild(userCard);
+}
