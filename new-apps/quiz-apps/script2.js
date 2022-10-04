f2287cb8a3afc868b74dca94aeab80828be686e5
oladips <oladips200@gmail.com>
2022-10-04 12:27:31 +0000

    worked done

--- new-apps/quiz-apps/script2.js
+++ new-apps/quiz-apps/script2.js
@@ -1,4 +1,18 @@
 /** @format */
+const quizContainer = document.querySelector(".quiz_container");
+const question = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submitBtn");
+const answersEl = document.querySelectorAll(".answer");
+const quizCont = document.getElementById("quizCont");
+const resultBox = document.getElementById("resultBox");
+const startBtn = document.getElementById("startBtn");
+const resultText = document.getElementById("resultText");
+const restartBtn = document.getElementById("restartBtn");
+
 const quizData = [
   {
     question: "What is the capital of India?",
@@ -50,33 +64,18 @@ const quizData = [
   },
 ];
 
-const question = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submitBtn");
-const answersEl = document.querySelectorAll(".answer");
-const quizCont = document.getElementById("quizCont");
-const resultBox = document.getElementById("resultBox");
-const startBtn = document.getElementById("startBtn");
-const resultText = document.getElementById("resultText");
-const restartBtn = document.getElementById("restartBtn");
-
+// ? starting the quiz
 startBtn.onclick = () => {
+  quizContainer.classList.add("active");
   startBtn.classList.add("hidden");
-  quizCont.classList.add("active");
 };
 
-restartBtn.onclick = () => {
-  window.location.reload();
-};
-
-let currrentQuiz = 0;
 let score = 0;
+let currrentQuiz = 0;
 
+// ?loading the quiz at the begining
 function loadQuiz() {
-  deselctAll();
+  deselectAll();
   const currrentQuizData = quizData[currrentQuiz];
   question.innerText = currrentQuizData.question;
   a_text.innerText = currrentQuizData.a;
@@ -85,26 +84,38 @@ function loadQuiz() {
   d_text.innerText = currrentQuizData.d;
 }
 
+// ? call the load quiz function
 loadQuiz();
 
+restartBtn.onclick = () => {
+  window.location.reload();
+};
+
+// ? getting users answer
 function getAnswer() {
   let userAnswer = undefined;
-  answersEl.forEach((answerEl) => {
-    if (answerEl.checked) {
-      userAnswer = answerEl.id;
+  answersEl.forEach((answer) => {
+    if (answer.checked) {
+      userAnswer = answer.id;
     }
   });
   return userAnswer;
 }
 
-function deselctAll() {
-  answersEl.forEach((answerEl) => {
-    answerEl.checked = false;
+// ? deselecting user answer
+function deselectAll() {
+  answersEl.forEach((answer) => {
+    answer.checked = false;
   });
 }
 
+// ? submitting the answer
 submitBtn.onclick = () => {
   const userAnswer = getAnswer();
+  if (!userAnswer) {
+    alert("please select an option");
+  }
+
   if (userAnswer === quizData[currrentQuiz].correct) {
     score++;
     console.log(score);
@@ -115,11 +126,11 @@ submitBtn.onclick = () => {
     if (currrentQuiz < quizData.length) {
       loadQuiz();
     } else {
-      quizCont.classList.remove("active");
+      quizContainer.classList.remove("active");
       resultBox.classList.add("active");
-      resultText.innerText = `You scored ${score} out of ${quizData.length} questions`;
+      const failedText = `You scored ${score} out of ${quizData.length} questions , try again`;
+      const passedText = `You scored ${score} out of ${quizData.length} questions , weldone.`;
+      resultText.innerText = score >= 4 ? passedText : failedText;
     }
-  } else {
-    alert("select an answer");
   }
 };
